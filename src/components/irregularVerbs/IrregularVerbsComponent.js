fc2409705d1f1ed4473d7c03e376f6286d288fc6
Joan Rueda <joanruedapau@gmail.com>
2020-04-29 13:06:45 +0200

    show Hide btn

--- src/components/irregularVerbs/IrregularVerbsComponent.js
+++ src/components/irregularVerbs/IrregularVerbsComponent.js
@@ -1,5 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import irregularVerbsList from "./IrregularVerbsList.json";
+import Button from "@material-ui/core/Button";
 
 const wrapperStyle = {
   display: "flex",
@@ -7,19 +8,26 @@ const wrapperStyle = {
   alignItems: "center",
 };
 
-// const irregularVerbsList = [
-//   { infinitive: "awake", simplePastTense: "awoke", pastParticiple: "awoken" },
-//   { infinitive: "be", simplePastTense: "was, were", pastParticiple: "been" },
-//   { infinitive: "bear", simplePastTense: "bore", pastParticiple: "born" },
-//   { infinitive: "beat", simplePastTense: "beat", pastParticiple: "beat" },
-// ];
-
 function IrregularVerbsComponent() {
+  const [fillTheGap, setFillTheGap] = useState(false);
+  const toggleFillTheGap = () => {
+    console.log("fillTheGap");
+    setFillTheGap(!fillTheGap);
+  };
   return (
     <div style={wrapperStyle}>
       <h1>
         List of <b>Irregular Verbs</b>
       </h1>
+      <Button
+        size="small"
+        variant="contained"
+        color="primary"
+        onClick={() => toggleFillTheGap()}
+      >
+        hide/show verbs
+      </Button>
+      <br />
       <table border="1" cellPadding="2" cellSpacing="2" bordercolor="#000000">
         <tbody>
           <tr>
@@ -33,13 +41,28 @@ function IrregularVerbsComponent() {
               <strong>Past Participle</strong>
             </th>
           </tr>
-          {irregularVerbsList.map((verb) => (
-            <tr key={verb.infinitive}>
-              <td>{verb.infinitive}</td>
-              <td>{verb.simplePastTense}</td>
-              <td>{verb.pastParticiple}</td>
-            </tr>
-          ))}
+          {irregularVerbsList.map((verb) => {
+            const min = 1;
+            const max = 3;
+            const showCol = Math.floor(Math.random() * (max - min + 1) + min);
+            if (fillTheGap) {
+              return (
+                <tr key={verb.infinitive}>
+                  <td>{showCol === 1 && verb.infinitive}</td>
+                  <td>{showCol === 2 && verb.simplePastTense}</td>
+                  <td>{showCol === 3 && verb.pastParticiple}</td>
+                </tr>
+              );
+            } else {
+              return (
+                <tr key={verb.infinitive}>
+                  <td>{verb.infinitive}</td>
+                  <td>{verb.simplePastTense}</td>
+                  <td>{verb.pastParticiple}</td>
+                </tr>
+              );
+            }
+          })}
         </tbody>
       </table>
     </div>
