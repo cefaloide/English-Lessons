b51d3058f93cdbc0ff2b836ba76a3becd0bc20d4
Joan Rueda <joanruedapau@gmail.com>
2020-05-04 18:07:54 +0200

    build

---
+++ precache-manifest.415403bd214514a34256b138df1c5536.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44fa2880d805a91e2803362a1655a9d6",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5c2977f979206312ad6a",
+    "url": "./static/css/main.f1b42223.chunk.css"
+  },
+  {
+    "revision": "f0157a88614c6096b9a0",
+    "url": "./static/js/2.d4283d41.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "./static/js/2.d4283d41.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c2977f979206312ad6a",
+    "url": "./static/js/main.9932a13e.chunk.js"
+  },
+  {
+    "revision": "b52f3b514ed8658e7817",
+    "url": "./static/js/runtime-main.dddad6cb.js"
+  }
+]);
